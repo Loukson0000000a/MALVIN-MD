df7eac2e3694fbc89b1f14c002eec151369dd8ec
Loukson0000000a <louksonl21@gmail.com>
2024-11-01 02:38:19 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -24,8 +24,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "⫷⛇𝑺𝑯𝑨𝑫𝑶𝑾࿙𝑲𝑨𝑹𝑨࿚𝑮𝑨𝑹𝑫𝑬𝑵⛇
-⫸",
+  OWNER_NAME: process.env.OWNER_NAME || "⫷⛇𝑺𝑯𝑨𝑫𝑶𝑾࿙𝑲𝑨𝑹𝑨࿚𝑮𝑨𝑹𝑫𝑬𝑵⛇⫸",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "221765147478",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
